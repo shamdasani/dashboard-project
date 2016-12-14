7ee44d1830a15edf656de250f976a4381387d975
Zach Latta <zach@zachlatta.com>
2016-12-13 18:20:59 -0800

    Fix indentation and remove extra whitespace

--- app.js
+++ app.js
@@ -1,53 +1,52 @@
 function getDate() {
-	var date = document.getElementById("date"); 
-	date.innerHTML = (new Date()).toString().split(' ').splice(0,4).join(' ');
+  var date = document.getElementById("date");
+  date.innerHTML = (new Date()).toString().split(' ').splice(0,4).join(' ');
 }
 
 getDate();
 
 function weather() {
 
-    var weather = document.getElementById("weather");
-    var url = 'https://api.forecast.io/forecast/';
-    var apiKey = '9f393b3649ff07769b75d4b7bc75fcd3'; 
+  var weather = document.getElementById("weather");
+  var url = 'https://api.forecast.io/forecast/';
+  var apiKey = '9f393b3649ff07769b75d4b7bc75fcd3';
 
-    navigator.geolocation.getCurrentPosition(success, error);
+  navigator.geolocation.getCurrentPosition(success, error);
 
-    function success(position) {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 
-       $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-        weather.innerHTML=('Based on your current location, it is ' + data.currently.temperature + '° F right now');
-      });
-    }
-
-    function error() {
-     alert("Unable to retrieve your location for weather");
-    }
-
-    weather.innerHTML = "fetching weather...";
+    $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
+      weather.innerHTML=('Based on your current location, it is ' + data.currently.temperature + '° F right now');
+    });
   }
+
+  function error() {
+    alert("Unable to retrieve your location for weather");
+  }
+
+  weather.innerHTML = "fetching weather...";
+}
 
 weather();
 
 
 function news() {
 
-	var news = document.getElementById("news");
-	var url = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=";
-	var apiKey = "83781e51c30e4a3bb7cc2b0ffde70d8c";
+  var news = document.getElementById("news");
+  var url = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=";
+  var apiKey = "83781e51c30e4a3bb7cc2b0ffde70d8c";
 
 
-	$.getJSON(url + apiKey, function(data) {
-		var titles = data.articles.map(function(articles) {
-                    return "<a href=" + articles.url + ">" + articles.title + "</a>";
-                  });
-		               
+  $.getJSON(url + apiKey, function(data) {
+    var titles = data.articles.map(function(articles) {
+      return "<a href=" + articles.url + ">" + articles.title + "</a>";
+    });
 
-        news.innerHTML=(titles.join("<br><br>"));
-      });
+    news.innerHTML=(titles.join("<br><br>"));
+  });
 
-	news.innerHTML = "fetching news..."
+  news.innerHTML = "fetching news..."
 }
 news();
